3bbe58338468dacab992f63a83dc39a579cc3725
NorbertLam <noobertlm@gmail.com>
2019-08-03 20:21:13 -0400

    remove view. add lookup to modelviewset

--- likes/api.py
+++ likes/api.py
@@ -4,23 +4,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .serializers import LikeSerializer
 
-class LikeViewSet(viewsets.ViewSet):
 
-  def list(self, request):
-    queryset = Like.objects.all()
-    serializer = LikeSerializer(queryset, many=True)
-    return Response(serializer.data)
-
-  def retrieve(self, request, pk=Like.slug):
-    queryset = Like.objects.all()
-    like = get_object_or_404(queryset, slug=pk)
-    serializer = LikeSerializer(like)
-    print(like)
-    return Response(serializer.data)
-
-# class LikeViewSet(viewsets.ModelViewSet):
-#   queryset = Like.objects.all()
-#   permission_classes = [
-#     permissions.AllowAny
-#   ]
-#   serializer_class = LikeSerializer
+class LikeViewSet(viewsets.ModelViewSet):
+  queryset = Like.objects.all()
+  lookup_field = 'slug'
+  permission_classes = [
+    permissions.AllowAny
+  ]
+  serializer_class = LikeSerializer
